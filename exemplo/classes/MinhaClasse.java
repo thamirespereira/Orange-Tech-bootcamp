f69999b79c85887fbdc8de970e9f50a5c721551f
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-01-28 17:26:34 +0000

    mudança no nome do arquivo

---
+++ exemplo/classes/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    
+/**
+ * @param args
+ * @throws Exception
+ */
+public static void main(String [] args) throws Exception {
+
+        System.out.println("Olá, mundo :D"); }
+
+}
