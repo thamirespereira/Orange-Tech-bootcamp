f291171e140baf627ba77b64f0a6bb44a5234a7e
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-01-30 23:37:39 +0000

    exemplo de operador ternário

---
+++ exemplo/operadores/operadorTernario.java
@@ -0,0 +1,14 @@
+package exemplo.operadores;
+
+public class operadorTernario {
+    public static void main(String [] args) throws Exception {
+    
+        int a, b;
+        a = 8;
+        b = 8;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+
+        System.out.print(resultado);
+    }
+}
