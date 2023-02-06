5e9614ccdf0eed7bae4a9528a21d4d4998bd15c7
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-06 22:15:29 +0000

    primeiro exercício com array

---
+++ exercicios/arrays/OrdemInversa.java
@@ -0,0 +1,22 @@
+package exercicios.arrays;
+
+public class OrdemInversa {
+    public static void main(String[] args) {
+        
+        int vetor [] = {-5, -6, 15, 50, 4, 8};
+
+        System.out.print("Vetor: ");
+        int count = 0;
+
+        while (count < (vetor.length)) {
+            System.out.print(vetor[count] + " ");
+
+            count++;
+        }
+
+        System.out.print("\nVetor: ");
+        for (int i = (vetor.length - 1); i >= 0; i--) {
+            System.out.print(vetor[i] + " ");
+        }
+    }
+}
