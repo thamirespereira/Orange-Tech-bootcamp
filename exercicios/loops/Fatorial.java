fd2c25838870f3e8ed3252a9b4a0abec379e834d
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-06 20:25:25 +0000

    exercício com fatorial

---
+++ exercicios/loops/Fatorial.java
@@ -0,0 +1,23 @@
+package exercicios.loops;
+
+import java.util.Scanner;
+
+public class Fatorial {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)) {
+
+            System.out.println("Fatorial: ");
+            int fatorial = scan.nextInt();
+
+            int multiplicacao = 1;
+
+            System.out.print(fatorial + "! = ");
+
+            for (int i = fatorial; i >=1; i--) {
+                multiplicacao = multiplicacao * i;
+            }
+        System.out.println(multiplicacao);
+        }
+
+    }
+}
