a06345a651655e08da33de087e8c7e32a4ae2ab0
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-13 17:18:44 +0000

    trabalhando com retornos

--- exercicios/metodos/Main.java
+++ exercicios/metodos/Main.java
@@ -30,5 +30,20 @@ public class Main {
         Quadrilatero.area(5d,5d);
         Quadrilatero.area(7,8,9);
         Quadrilatero.area(5f,5f);
+
+        //Retornos
+        System.out.println("Exercício retornos");
+
+        double areaQuadrado = Quadrilatero2.area(3);
+        System.out.println("Área do quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero2.area(5d, 5d);
+        System.out.println("Área do retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero2.area(7,8,9);
+        System.out.println("Área do trapézio: " + areaTrapezio);
+
+        float areaLosango = Quadrilatero2.area(5f, 5f);
+        System.out.println("Área do losango: " + areaLosango);
     }
 }
