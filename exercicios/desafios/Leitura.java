79a604e3e27e1dcce0fe37fd8fc2e4b133ef7e81
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-15 17:04:22 +0000

    terceiro desafio

---
+++ exercicios/desafios/Leitura.java
@@ -0,0 +1,18 @@
+package exercicios.desafios;
+
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Páginas: ");
+        int paginas = leitor.nextInt();
+        int paginasLidas = 3;
+        int dias = paginas / paginasLidas;
+        
+        System.out.println(dias + " dias");
+        leitor.close();
+    }
+    
+}
