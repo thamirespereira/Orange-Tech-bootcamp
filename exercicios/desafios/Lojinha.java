a202902bdf3d0ea605979c03ca24d8bbfc0194f8
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-15 17:57:51 +0000

    quarto desafio

---
+++ exercicios/desafios/Lojinha.java
@@ -0,0 +1,20 @@
+package exercicios.desafios;
+
+import java.util.Scanner;
+
+public class Lojinha {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Dinheiro: ");
+        int dinheiro = leitor.nextInt();
+        int doces = 2;
+        int quantidadeDeDoces = doces * dinheiro;
+        
+        System.out.println("O cliente obteve " + quantidadeDeDoces + " doces");
+        
+        leitor.close();
+        // TODO: Crie a condição necessária para que cada cliente saiba quantos doces vai obter de acordo com a entrada
+
+    }
+}
