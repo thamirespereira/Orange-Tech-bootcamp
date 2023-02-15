ba1e16fcb174e86f143bee0692780973d01614ed
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-15 14:26:42 +0000

    primeiro desafio

---
+++ exercicios/desafios/Program.java
@@ -0,0 +1,22 @@
+package exercicios.desafios;
+
+import java.util.Scanner;
+    public class Program {
+     
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int mesada = 50;
+
+        System.out.println("Entrada: ");
+        int entrada = leitor.nextInt();
+       
+        System.out.println("Valor total: " + entrada * mesada);
+
+        leitor.close();
+
+    //TODO: Crie a condição necessária para que o sobrinho saiba quanto dinheiro irá juntar nos meses da entrada
+
+    }
+}
+
+
