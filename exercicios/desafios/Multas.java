8119c5be435aa4986d4c41181c9436b495c55761
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-02-15 18:05:51 +0000

    quinto desafio

---
+++ exercicios/desafios/Multas.java
@@ -0,0 +1,21 @@
+package exercicios.desafios;
+
+import java.util.Scanner;
+
+public class Multas {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Velocidade: ");
+        int velocidadeAtual = leitor.nextInt();
+        
+        if (velocidadeAtual > 60)
+          System.out.println("Foi multado");
+        
+        else 
+          System.out.println("Nao foi multado");
+        
+        // TODO: Crie a condição necessária para verificar se o motorista, de acordo com a entrada, foi multado ou não.
+        leitor.close();
+    }
+}
