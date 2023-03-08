c86dc9aebce57b3732bc646a65063593714adb3a
Thamires Pereira <90210266+thamirespereira@users.noreply.github.com>
2023-03-08 22:14:56 +0000

    exercício temperatura

--- exercicios/collections/List/ExercicioTemperatura.java
+++ exercicios/collections/List/ExercicioTemperatura.java
@@ -1,6 +1,7 @@
 package exercicios.collections.List;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -34,10 +35,48 @@ public class ExercicioTemperatura {
          double tempMedia = soma / mes;
          System.out.println("Temperatura média: " + tempMedia);
 
+         //temperatura acima da média
+         System.out.print("Temperaturas acima da média: ");
+         temperaturas.stream()
+                 .filter(t -> t > tempMedia)
+                 .forEach(t -> System.out.printf("%.1f ", t));
+        System.out.println();
 
+        //mostrar temperatura e mes
+        Iterator<Double> iterator = temperaturas.iterator();
+
+        int count = 0;
+        while (iterator.hasNext()) {
+            Double temp = iterator.next();
+            if (temp > tempMedia) {
+                switch (count) {
+                    case (0):
+                        System.out.printf("1 - janeiro: %.1f\n ", temp);
+                        break;
+                    case (1):
+                        System.out.printf("2 - fevereiro: %.1f\n", temp);
+                        break;
+                    case (2):
+                        System.out.printf("3 - março: %.1f\n", temp);
+                        break;
+                    case (3):
+                        System.out.printf("4 - abril: %.1f\n", temp);
+                        break;
+                    case (4):
+                        System.out.printf("5 - maio: %.1f\n", temp);
+                        break;
+                    case (5):
+                        System.out.printf("6 - junho: %.1f\n", temp);
+                        break;
+                    default:
+                        System.out.println("Não houve temperatura acima da média.");
+                }
+            }
+            count++;
 
          leitor.close();
 
     }
     
+}
 }
